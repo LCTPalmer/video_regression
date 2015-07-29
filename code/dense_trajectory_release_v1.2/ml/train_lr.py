0d5a1fef1014809bccb2f32c4f1c35ac38ddf3c3
Luke <lctpalmer@gmail.com>
2015-07-29 22:30:29 +0100

    updated training

--- code/dense_trajectory_release_v1.2/ml/train_lr.py
+++ code/dense_trajectory_release_v1.2/ml/train_lr.py
@@ -1,7 +1,7 @@
 #train a regressor
 import numpy as np
 from sklearn import cross_validation
-from sklearn.preprocessing import OneHotEncoder, normalize
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder, normalize
 from sklearn.externals import joblib
 from sklearn.linear_model import LogisticRegression
 from hyperopt import hp, fmin, tpe, space_eval, Trials
@@ -17,35 +17,33 @@ dt = joblib.load(dataset_file)
 
 #the objective for hyperopt to minimise
 def objective(args, data):
-	l2_param = args
+	(reg_type, reg_param) = args
 	#define the model
-	model = LogisticRegression(penalty='l2', C=l2_param)
+	model = LogisticRegression(penalty=reg_type, C=reg_param)
 	#get score from cv on training data
 	scores = cross_validation.cross_val_score(model, data['features'], data['labels'], cv=3)
-	error_rate = 1 - scores.mean()
+	error_rate = 1-scores.mean()
 	return error_rate #minimise the error rate
 
 #set the space
-space = [(hp.uniform('l2_param', 0, 1))]
+space = [hp.choice('reg', ['l1', 'l2']), hp.uniform('reg_param', 0, 100)]
 	
 #search
 trials = Trials()
-c = 10
 
 #prepare the data
 data = {}
 X = np.array(dt.feature_dict['Trajectory']); X = normalize(X, axis=1, norm='l2')
-print X.shape
 y = dt.feature_dict['Label']; y = [y[el][0] for el in range(len(y))]; y = np.array(y)
-print y.shape, y[:5]
+le = LabelEncoder()
+y_t = le.fit_transform(y)
 data['features'] = X
-data['labels'] = y
+data['labels'] = y_t
 
-best = fmin(fn=lambda args: objective(args,data), space=space, algo=tpe.suggest, max_evals=2, trials=trials)
-
+best = fmin(fn=lambda args: objective(args,data), space=space, algo=tpe.suggest, max_evals=200, trials=trials)
+loss_hist = [trials.trials[t]['result']['loss'] for t in range(len(trials.trials))]
 #report results
 print best
-print space_eval(space, best)
 #print trials.trials
 
 '''
